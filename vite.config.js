af76dbc94ad115514247392a601a1c8460ffa706
Juan Manuel <juanmanuelccampos@gmail.com>
2025-08-26 21:19:10 -0300

    dominio groovecafe.com.ar

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/groove_web/',
+  base: '/', // Cambiado para dominio personalizado
   build: {
     minify: 'terser',
     terserOptions: {
